3398dde1bab6c5d2cf6ff5e61b30a2f645cdf25d
Lavesh Singhal <121092737+laveshs03@users.noreply.github.com>
2022-12-20 15:34:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
-streamlit.header('food menu')
-streamlit.text('just order whatever you want')
-
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale, spinach &rocket smoothie')
+stramlit.text('hard-boiled free-range agg')
